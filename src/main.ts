a1390a6c0bb24e28fb1a71d4d03447c739e12c26
parlimpim <kolo24045@gmail.com>
2024-07-09 16:37:24 +0700

    setup db container, swagger, env and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const API_DEFAULT_PORT = 8080;
+
+const SWAGGER_TITLE = 'Concert App API';
+const SWAGGER_DESCRIPTION = 'API used for Concert App';
+const SWAGGER_PREFIX = 'api-docs';
+
+function createSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle(SWAGGER_TITLE)
+    .setDescription(SWAGGER_DESCRIPTION)
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(SWAGGER_PREFIX, app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // create swagger
+  createSwagger(app);
+
+  // enable cors
+  app.enableCors({ origin: process.env.ALLOW_DOMAIN, credentials: true });
+
+  await app.listen(process.env.API_PORT || API_DEFAULT_PORT);
 }
 bootstrap();
