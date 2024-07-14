0630e3d61fb73216d26bf7076960db4e4459ce69
parlimpim <kolo24045@gmail.com>
2024-07-14 21:44:41 +0700

    add unit test to interceptor

--- src/interceptors/clear-tokens.interceptor.spec.ts
+++ src/interceptors/clear-tokens.interceptor.spec.ts
@@ -1,7 +1,48 @@
+import { CallHandler, ExecutionContext } from '@nestjs/common';
 import { ClearTokensInterceptor } from './clear-tokens.interceptor';
+import { of } from 'rxjs';
+import { Response } from 'express';
 
 describe('ClearTokensInterceptor', () => {
+  let interceptor: ClearTokensInterceptor;
+
+  beforeEach(() => {
+    interceptor = new ClearTokensInterceptor();
+  });
+
   it('should be defined', () => {
     expect(new ClearTokensInterceptor()).toBeDefined();
+  });
+
+  it('response should not have tokens in cookie', () => {
+    const mockExecutionContext = {
+      switchToHttp: jest.fn().mockReturnValue({
+        getResponse: jest.fn().mockReturnValue({
+          cookie: jest.fn(),
+        }),
+      }),
+    } as unknown as ExecutionContext;
+
+    const mockCallHandler: CallHandler = {
+      handle: () => of({}),
+    };
+
+    interceptor
+      .intercept(mockExecutionContext, mockCallHandler)
+      .subscribe(() => {
+        const response: Response = mockExecutionContext
+          .switchToHttp()
+          .getResponse();
+
+        // should clear tokens in cookie
+        expect(response.clearCookie).toHaveBeenCalledWith(
+          'access_token',
+          expect.objectContaining({}),
+        );
+        expect(response.clearCookie).toHaveBeenCalledWith(
+          'refresh_token',
+          expect.objectContaining({}),
+        );
+      });
   });
 });
--- src/interceptors/response.interceptor.spec.ts
+++ src/interceptors/response.interceptor.spec.ts
@@ -1,7 +1,84 @@
+import { createMock } from '@golevelup/ts-jest';
+import { CallHandler, ExecutionContext } from '@nestjs/common';
 import { ResponseInterceptor } from './response.interceptor';
+import { lastValueFrom, of } from 'rxjs';
+import { Response } from 'express';
 
 describe('ResponseInterceptor', () => {
+  let interceptor: ResponseInterceptor;
+
+  beforeEach(() => {
+    interceptor = new ResponseInterceptor();
+  });
+
   it('should be defined', () => {
     expect(new ResponseInterceptor()).toBeDefined();
+  });
+
+  it('should return correct response format', async () => {
+    const data: any = {
+      message: 'Login Successful',
+      user: { id: '1', name: 'user 01' },
+    };
+
+    const context = createMock<ExecutionContext>();
+    const handler = createMock<CallHandler>({
+      handle: () => of(data),
+    });
+
+    const responseObservable = interceptor.intercept(context, handler);
+    const response = await lastValueFrom(responseObservable);
+
+    // should have success attribute
+    expect(response).toHaveProperty('success');
+    expect(response.success).toEqual(true);
+  });
+
+  it('should set tokens in cookie if data have tokens', () => {
+    const data = {
+      message: 'Login Successful',
+      user: { id: '1', name: 'user 01' },
+      tokens: {
+        accessToken: 'access token',
+        refreshToken: 'refresh token',
+      },
+    };
+
+    const mockExecutionContext = {
+      switchToHttp: jest.fn().mockReturnValue({
+        getResponse: jest.fn().mockReturnValue({
+          cookie: jest.fn(),
+        }),
+      }),
+    } as unknown as ExecutionContext;
+
+    const mockCallHandler: CallHandler = {
+      handle: () => of(data),
+    };
+
+    const { tokens: _, ...dataWithoutTokens } = data;
+    const response = { success: true, ...dataWithoutTokens };
+
+    interceptor
+      .intercept(mockExecutionContext, mockCallHandler)
+      .subscribe((result) => {
+        // should return response without tokens
+        expect(result).toEqual(response);
+
+        const res: Response = mockExecutionContext.switchToHttp().getResponse();
+
+        // should have tokens in cookie
+        expect(res.cookie).toHaveBeenCalledWith(
+          'access_token',
+          data.tokens.accessToken,
+          expect.objectContaining({}),
+        );
+
+        expect(res.cookie).toHaveBeenCalledWith(
+          'refresh_token',
+          data.tokens.refreshToken,
+          expect.objectContaining({}),
+        );
+      });
   });
 });
